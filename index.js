b54b8011979868f2aa2c3c0c4bba934b6742c36e
Himanshu <hs469908@gmail.com>
2021-04-08 00:06:48 +0530

    first update

--- index.js
+++ index.js
@@ -2,46 +2,46 @@ const express = require('express');
 const app = express();
 const path = require('path')
 app.use(express.urlencoded({extended:true}))
+//parses form data into json//
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'views'))
 
 
+
 const comments = [
-    {username:'Dante',
-    comment:'cocky'
-},
-{username:'Dante111',
+    {
+        username:'Dante',
+        comment:'cocky'
+    },
+    {
+    username:'Dante111',
     comment:'cocky111'
-},{username:'Dante2222',
-comment:'cocky333'
-}
+    },
+    {
+    username:'Dante2222',
+    comment:'cocky333'
+    }
 ];
 
-app.post('/comments',(req,res)=>{
-    console.log(req.body);
-const{username,comment} = req.body;
-comments.push({username,comment});
-res.redirect('comments');
+
+
+app.get('/comments', (req,res)=>{
+    res.render('comments/index',{comments})
 })
 
 app.get('/comments/new',(req,res)=>{
-    res.render('comments/new')
+    res.render('comments/new'); 
 })
 
-app.get('/comments',(req,res)=>{
-    res.render('comments/index',{comments});
+app.post('/comments',(req,res)=>{
+    const {username, comment} = req.body;
+    comments.push({username,comment})
+    res.redirect('/comments')
 })
 
-// app.get('/:ninja',(req,res) => {
-//     const {ninja: id} = req.params;
-//     console.log(req.params);
-//     res.send(`this is my response ${id}`);
-// })
-
-// app.post('/ninja',(req,res) => {
-//     res.send("this is my post response")
-//     console.log(req.body);
-// })
+app.get('*',(req,res)=>{
+    res.send(`this is wrong page!! mf run`)
+})
 
 app.listen(3000,()=> {
     console.log("Port 3000 live")
